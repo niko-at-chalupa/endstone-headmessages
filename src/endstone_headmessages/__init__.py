3810187d7cb868e6cc1b0c32e90e3203b9e226dd
niko-at-chalupa <181960037+niko-at-chalupa@users.noreply.github.com>
2026-05-01 02:01:25 +0800

    initial commit

---
+++ src/endstone_headmessages/__init__.py
@@ -0,0 +1,3 @@
+from .main import HeadMessages
+
+__all__ = ["HeadMessages"]
---
+++ src/endstone_headmessages/main.py
@@ -0,0 +1,40 @@
+from endstone.plugin import Plugin
+from endstone.event import event_handler, PlayerChatEvent, PlayerJoinEvent, PlayerQuitEvent
+from endstone import Player, event
+from endstone import ColorFormat as cf
+
+class HeadMessages(Plugin):
+    def on_enable(self):
+        self.register_events(self)
+
+        self.player_messages: dict[Player, list[str]] = {}
+
+    @event_handler
+    def on_player_join(self, event: PlayerJoinEvent):
+        player = event.player
+        self.player_messages[player] = []
+
+    @event_handler
+    def on_player_chat(self, event: PlayerChatEvent):
+        player = event.player
+        message = event.message
+        
+        if player not in self.player_messages:
+            self.player_messages[player] = []
+        self.player_messages[player].append(message)
+        
+        messages_text = "\n".join(self.player_messages[player][-3:])
+        event.player.name_tag = f"{cf.GRAY}<{player.name}>\n\n{messages_text}"
+        
+        def clear_message():
+            if player in self.player_messages and self.player_messages[player]:
+                self.player_messages[player].pop()
+                if self.player_messages[player]:
+                    messages_text = "\n".join(self.player_messages[player][-3:])
+                    player.name_tag = f"{cf.GRAY}{player.name}{cf.RESET}\n\n{messages_text}"
+                else:
+                    player.name_tag = player.name
+        
+        self.server.scheduler.run_task(plugin=self, task=clear_message, delay=700)
+
+        self.server.broadcast_message(f"{event.player.name}.name_tag=\n{event.player.name_tag}")
