cb00a7cad609181219b941828a91b8c7fd6837d0
niko-at-chalupa <181960037+niko-at-chalupa@users.noreply.github.com>
2026-05-01 22:00:32 +0800

    Enhance config, add dynamic delay

--- src/endstone_headmessages/main.py
+++ src/endstone_headmessages/main.py
@@ -24,6 +24,8 @@ class HeadMessages(Plugin):
             ("message_wrapping", True, "Weather to use message wrapping or truncation."),
             ("player_name_color", "RESET", "Player name colour by default (i.e., no messages). Use Endstone ColorFormat colours ( <https://endstone.dev/latest/reference/python/misc/#endstone.ColorFormat> )"),
             ("player_name_color_messages", "GRAY", "Player name colour with messages. Use Endstone ColorFormat colours ( <https://endstone.dev/latest/reference/python/misc/#endstone.ColorFormat> )"),
+            ("dynamic_decay", True, "Weather to enable dynamic durations. Minimum duration is the regular message decay."),
+            ("ticks_per_char", 1, "ticks/char to be added onto the message decay (20 chars is 20 ticks = 1 second at 20 TPS). If a message is 40 chars (somewhat long) at 1 tick / char and the regular message decay is 700 the decay will be 740 ticks (1 * chars + message decay)")
         ]
         if cfg_path.exists():
             with open(cfg_path, "r", encoding="utf-8") as f:
@@ -48,14 +50,21 @@ class HeadMessages(Plugin):
 
         self.install()
         try:
-            self.max_messages = cast(int, self.yaml_config.get("max_messages"))
-            self.message_decay = cast(int, self.yaml_config.get("message_decay"))
-            self.threshold = cast(int, self.yaml_config.get("threshold"))
-            self.message_wrapping = cast(bool, self.yaml_config.get("message_wrapping"))
-            self.player_name_color = getattr(cf, cast(str, self.yaml_config.get("player_name_color", "RESET")), cf.RESET)
-            self.player_name_color_messages = getattr(cf, cast(str, self.yaml_config.get("player_name_color_messages", "GRAY")), cf.GRAY)
-        except Exception:
-            self.logger.error("Your config is bad! Delete it to generate a new one.")
+            self.max_messages = int(self.yaml_config["max_messages"])
+            self.message_decay = int(self.yaml_config["message_decay"])
+            self.threshold = int(self.yaml_config["threshold"])
+            
+            self.message_wrapping = bool(self.yaml_config["message_wrapping"])
+            self.dynamic_delay = bool(self.config["dynamic_delay"])
+            self.ticks_per_char = bool(self.config["ticks_per_char"])
+
+            p_color = str(self.yaml_config.get("player_name_color", "RESET")).upper()
+            self.player_name_color = getattr(cf, p_color, cf.RESET)
+
+            m_color = str(self.yaml_config.get("player_name_color_messages", "GRAY")).upper()
+            self.player_name_color_messages = getattr(cf, m_color, cf.GRAY)
+        except (KeyError, ValueError, TypeError) as e:
+            self.logger.error(f"Configuration error: {e}. Delete your config to generate a new one.")
             return
         self.register_events(self)
     
@@ -102,4 +111,7 @@ class HeadMessages(Plugin):
                 self.player_messages[player].pop(0)
             update_tag()
         
-        self.server.scheduler.run_task(plugin=self, task=clear_message, delay=self.message_decay)
+        decay = self.message_decay
+        if self.dynamic_delay:
+            decay = 1 * self.ticks_per_char + self.message_decay
+        self.server.scheduler.run_task(plugin=self, task=clear_message, delay=decay)
